af822d3061718dbad8653d309c318a867da5bf81
Thomas Skerbis <thomas.skerbis@mac.com>
2025-01-16 20:11:51 +0100

    Update boot.php

--- boot.php
+++ boot.php
@@ -3,17 +3,15 @@ if (rex::isBackend() && rex::getUser()) {
     // Template-Pfad für YFORM registrieren
     rex_yform::addTemplatePath($this->getPath('ytemplates'));
 
-    // Assets von Gridblock bei Bedarf einbinden
+    // Nur die Gridblock CSS Datei einbinden, wenn wir im YFORM Edit sind
     if (rex_request('page') === 'yform/manager/data_edit') {
         $addon = rex_addon::get('gridblock');
         rex_view::addCssFile($addon->getAssetsUrl('style.css'));
         if ($addon->getProperty('darkmode')) {
             rex_view::addCssFile($addon->getAssetsUrl('style-darkmode.css'));
         }
-        rex_view::addJsFile($addon->getAssetsUrl('sortable.min.js'));
-        rex_view::addJsFile($addon->getAssetsUrl('script.js'));
     }
 }
 
-// Klasse für YFORM registrieren
+// Klasse für YFORM registrieren  
 rex_yform::addValueField('gridblock', 'YForm_Gridblock_Value');
